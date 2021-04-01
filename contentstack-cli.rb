66a0a4e4223f371d12b485b0add96797279298aa
Rohit Mishra <rohit.mishra@contentstack.com>
2021-04-01 16:13:32 +0530

    made changes in rb file

--- contentstack-cli.rb
+++ contentstack-cli.rb
@@ -1,17 +1,19 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 require "language/node"
-class Cli < Formula
-  desc "Contentstack Command-line Interface (CLI)"
-  homepage ""
+class ContentstackCli < Formula
+  desc "Command-line tool (CLI) to interact with Contentstack"
+  homepage "https://github.com/contentstack/cli"
   url "https://registry.npmjs.org/@contentstack/cli/-/cli-0.1.1-beta.5.tgz"
   sha256 "f9e96c14cd538999a4d9d580c828a059bac1ec86fbfedac5bba6ee0e1c68e388"
   license "MIT"
-
+  livecheck do
+    url :stable
+  end
+  depends_on "node"
   def install
-    # system "make", "install"
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+  test do
+    raise "Test not implemented."
   end
 end
